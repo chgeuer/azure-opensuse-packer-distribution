c5baf9a192f3c80e6f7b6702d41113aa8f08bcc8
Christian Geuer-Pollmann <chgeuer@users.noreply.github.com>
2018-04-20 07:47:34 +0200

    Update distribution.sh
    Validated w/ https://www.shellcheck.net/#

--- distribution.sh
+++ distribution.sh
@@ -40,9 +40,9 @@ SourceAccountKey=$(az storage account keys list \
        --o              tsv)
 
 declare -A StorageAccountKeys
-for regionname in ${!StorageAccountNames[@]} 
+for regionname in "${!StorageAccountNames[@]}"
 do 
-   local key=$(az storage account keys list \
+   key=$(az storage account keys list \
        --resource-group "${resourceGroup}" \
        --account-name   "${StorageAccountNames[$regionname]}" \
        --query          "[?contains(keyName,'key1')].[value]" \
@@ -54,9 +54,9 @@ done
 #
 # Create Storage Account Containers
 #
-for regionname in ${!StorageAccountNames[@]} 
+for regionname in "${!StorageAccountNames[@]}"
 do 
-    local created=$(az storage container create \
+    created=$(az storage container create \
         --account-name "${StorageAccountNames[$regionname]}" \
         --account-key  "${StorageAccountKeys[$regionname]}" \
         --name         "${targetContainerName}" \
@@ -64,7 +64,7 @@ do
         --o             tsv \
         --query "created" )
  
-    if [[ "true" == $created ]]; then
+    if [[ "true" == "$created" ]]; then
         echo "Created container ${StorageAccountNames[$regionname]}/${targetContainerName} in region ${regionname}"
     else
         echo "Container ${StorageAccountNames[$regionname]}/${targetContainerName} already existed in region ${regionname}"
@@ -75,9 +75,9 @@ done
 # Trigger copy operations
 #
 declare -A CopyOperationIDs
-for regionname in ${!StorageAccountNames[@]} 
+for regionname in "${!StorageAccountNames[@]}" 
 do 
-    local copyOperationId=$(az storage blob copy start \
+    copyOperationId=$(az storage blob copy start \
         --source-account-name   "${SourceAccountName}" \
         --source-account-key    "${SourceAccountKey}" \
         --source-container      "${SourceContainer}" \
@@ -93,9 +93,9 @@ do
 done
 
 while [[ ${#CopyOperationIDs[@]} -gt 0 ]]; do
-    for regionname in ${!CopyOperationIDs[@]} 
+    for regionname in "${!CopyOperationIDs[@]}"
     do 
-        local status=$(az storage blob show \
+        status=$(az storage blob show \
             --account-name   "${StorageAccountNames[$regionname]}" \
             --account-key    "${StorageAccountKeys[$regionname]}" \
             --container-name "${targetContainerName}" \
@@ -104,9 +104,9 @@ while [[ ${#CopyOperationIDs[@]} -gt 0 ]]; do
             --query          "properties.copy.[status]" ) # "properties.copy.[status, progress]" )
 
         dest="${StorageAccountNames[$regionname]}/${targetContainerName}/${SourceBlob}"
-        if [[ "success" == $status ]]; then
+        if [[ "success" == "$status" ]]; then
             echo "Finished ${dest}, removing from list"
-            unset CopyOperationIDs[$regionname]
+            unset "${CopyOperationIDs[$regionname]}"
         else
             # echo "Still working on ${dest}, $(progress_to_percent "${progress}")%"
             progress=$(az storage blob show \
@@ -124,9 +124,9 @@ done
 
 echo "Finished all copy operations, copied the VHD to ${#StorageAccountNames[@]} regions"
 
-for regionname in ${!StorageAccountNames[@]} 
+for regionname in "${!StorageAccountNames[@]}"
 do 
-    local vhdUrl=$(az storage blob url \
+    vhdUrl=$(az storage blob url \
         --protocol "https" \
         --account-name   "${StorageAccountNames[$regionname]}" \
         --account-key    "${StorageAccountKeys[$regionname]}" \
@@ -134,12 +134,12 @@ do
         --name           "${SourceBlob}" \
         --o tsv)
 
-    local imagename="${regionname}-${SourceBlob}"
-    local regionTag="${RegionTag[$regionname]}"
+    imagename="${regionname}-${SourceBlob}"
+    regionTag="${RegionTag[$regionname]}"
     
     echo "Creating image ${imagename} in ${regionname}, tagging it with datacenterID=${regionTag}. VHD is ${vhdUrl}"
 
-    local provisioningState=$(az image create \
+    provisioningState=$(az image create \
         --name           "${imagename}" \
         --resource-group "${resourceGroup}" \
         --location       "${regionname}" \
